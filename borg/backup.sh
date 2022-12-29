789a863c978793c572bb8de2fc6e7a7f51eed501
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2022-12-29 18:43:54 +0100

    Added borg to backup docker data

---
+++ borg/backup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# INITIALIZE THE REPO WITH THE COMMAND:
+#   borg init --encryption=none /var/borg/repository
+# THEN RUN THIS SCRIPT
+
+# -----------------------------------------------
+
+BACKUP_THIS='/var/docker'
+REPOSITORY='/var/borg/repository'
+LOGFILE='/var/borg/borg.log'
+S3_BUCKET=backup-s1-rn
+S3_PROFILE=default
+
+
+# -----------------------------------------------
+
+NOW=$(date +"%Y-%m-%d | %H:%M | ")
+echo "$NOW Starting Backup and Prune" >> $LOGFILE
+
+# CREATES NEW ARCHIVE IN PRESET REPOSITORY
+
+borg create                                     \
+    $REPOSITORY::'{now:%s}'                     \
+    $BACKUP_THIS                                \
+                                                \
+    --compression zstd                          \
+    --one-file-system                           \
+    --exclude-caches                            \
+    --exclude-if-present '.nobackup'            \
+
+# DELETES ARCHIVES NOT FITTING KEEP-RULES
+
+borg prune -v --list $REPOSITORY                \
+    --keep-daily=7                              \
+    --keep-weekly=4                             \
+    --keep-monthly=6                            \
+    --keep-yearly=0                             \
+
+echo "$NOW Starting to backup repo to S3" >> $LOGFILE
+
+# BACKUP REPO TO S3
+aws s3 sync $REPOSITORY s3://$S3_BUCKET --profile=$S3_PROFILE --delete
+
+echo "$NOW Done" >> $LOGFILE
+echo '------------------------------' >> $LOGFILE
+
+# --- USEFULL SHIT ---
+
+# setup above ignores directories containing '.nobackup' file
+# make '.nobackup' imutable using chattr to prevent accidental removal
+#   touch .nobackup
+#   chattr +i .nobackup
+
+# in the repo folder, to list available backups:
+#   borg list .
+# to mount one of them:
+#   borg mount .::1584472836 ~/temp
+# to umount:
+#   borg umount ~/temp
+# to delete single backup in a repo:
+#   borg delete .::1584472836
---
+++ borg/readme.md
@@ -0,0 +1,24 @@
+# Borg backup docker directories and volumes
+
+0. Install and configure awscli and s3 bucket
+```bash
+sudo apt-get install awscli
+sudo apt-get install pip
+pip3 install awscli-plugin-endpoint
+```
+
+Configure the bucket [scaleway](https://www.scaleway.com/en/docs/storage/object/api-cli/object-storage-aws-cli/)
+
+1. Copy the script into /var/borg
+2. Add the script to the crontab to backup at 00:00 and 12:00 (server timestamp)
+
+```txt
+0 0 * * * /var/borg/backup.sh
+0 12 * * * /var/borg/backup.sh
+```
+
+## Resources
+- [https://github.com/luispabon/borg-s3-home-backup/blob/master/borg-backup.sh](https://github.com/luispabon/borg-s3-home-backup/blob/master/borg-backup.sh)
+- [https://github.com/DoTheEvo/selfhosted-apps-docker/tree/master/borg_backup](https://github.com/DoTheEvo/selfhosted-apps-docker/tree/master/borg_backup)
+
+https://backup-s1-rn.s3.fr-par.scw.cloud
